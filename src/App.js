fb0096817bd5b4bbb410e815f09b427e55316a5f
leeyang19970916 <yang19970916@gmail.com>
2023-04-10 17:04:04 +0800

    address color

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-// import Login from "./pages/login";
 import React, { Fragment, useState, useEffect, useRef } from "react";
 import Navbar from "./component/navbar";
 import Home from "./pages/home";
@@ -12,13 +11,10 @@ import {
 import ClinicDetail from "./component/home/clinic-detail";
 import Approved from "./pages/approved";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-
 import UserLogin from "./pages/login";
 import "./scss/App.scss";
 import { useSelector } from "react-redux";
-
 import { useViewport } from "./useViewport";
-
 const NavbarWidth = {
   left: `0`,
 };
@@ -31,14 +27,11 @@ function App() {
   useEffect(() => {
     setIsLogin(isLogin_store);
   }, [isLogin_store]);
-  const goPath = useHistory(); //設常數接收useHistory()回傳的物件
+  const goPath = useHistory();
 
   let headNavbarRef = useRef() || "";
   const style = {
     height: `calc(100% - 45px)`,
-    // height: `calc(100vh - 45px - ${toolbarHeight}px)`,
-    // height:"100vh",
-    // height:"100%",
     position: `relative`,
   };
   const menuHandler = () => {
@@ -46,7 +39,6 @@ function App() {
   };
   const goHome = () => {
     goPath.push(`/`);
-
   };
   const elementRef = useRef(null);
   useEffect(() => {
@@ -66,7 +58,7 @@ function App() {
   const location = useLocation();
   useEffect(() => {
     window.scrollTo(0, 0);
-  }, [location.pathname]); 
+  }, [location.pathname]);
   useEffect(() => {
     if (window.screen.orientation && window.screen.orientation.lock) {
       window.screen.orientation.lock("portrait").catch(function () {
@@ -76,7 +68,6 @@ function App() {
   }, []);
   return (
     <Fragment>
-      {/* navbar */}
       {!isLogin ? (
         <div ref={headNavbarRef}></div>
       ) : (
@@ -85,10 +76,7 @@ function App() {
           style={{ height: "45px", borderBottom: "1px solid black" }}
           className="bg-dark text-white py-2 px-3 d-flex justify-content-between align-items-center"
         >
-          <div
-            className="h4 m-0 cursor-pointer "
-            onClick={goHome}
-          >
+          <div className="h4 m-0 cursor-pointer " onClick={goHome}>
             診所管理系統
           </div>
           {!menuIsShow && (
@@ -103,20 +91,10 @@ function App() {
           )}
         </div>
       )}
-      {/* main */}
       {!isLogin && (
-        <div
-          className="w-100 d-flex heightSetting h-100 "
-          ref={elementRef}
-          
-        >
+        <div className="w-100 d-flex heightSetting h-100 " ref={elementRef}>
           <div className="bg-light h-100 w-100 flex-wrap  ">
-            <Route
-              exact
-              path="/login"
-              // onEnter={() => window.scrollTo(0, 0)}
-              component={UserLogin}
-            ></Route>
+            <Route exact path="/login" component={UserLogin}></Route>
             <Redirect to="/login" />
           </div>
         </div>
@@ -134,28 +112,10 @@ function App() {
           <div className="bg-light h-100 w-100 flex-wrap  RouterWidth">
             <Switch>
               <Route path="/login" component={UserLogin} />
-
-              <Route
-                exact
-                path="/"
-                // onEnter={() => window.scrollTo(0, 0)}
-                component={Home}
-              ></Route>
-              <Route
-                path="/clinic/:id"
-                // onEnter={() => window.scrollTo(0, 0)}
-                component={ClinicDetail}
-              />
-              <Route
-                path="/approved"
-                // onEnter={() => window.scrollTo(0, 0)}
-                component={Approved}
-              />
-              <Route
-                path="/approved/:id"
-                // onEnter={() => window.scrollTo(0, 0)}
-                component={Approved}
-              />
+              <Route exact path="/" component={Home}></Route>
+              <Route path="/clinic/:id" component={ClinicDetail} />
+              <Route path="/approved" component={Approved} />
+              <Route path="/approved/:id" component={Approved} />
             </Switch>
           </div>
         </div>
