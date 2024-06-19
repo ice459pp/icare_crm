59a585b7b1650ea4f6c7323c505f5e737db9df91
ice459pp <ice459pp@gmail.com>
2024-06-19 13:31:18 +0800

    add ckeditor lib

--- src/pages/approved.js
+++ src/pages/approved.js
@@ -4,6 +4,7 @@ import "../scss/approved.scss";
 import ApprovedItem from "../component/approved/approved-item";
 import { useHistory } from "react-router-dom";
 import { useSelector } from "react-redux";
+import MyCKEditor from '../component/ckeditor/MyCKEditor';
 
 let arrayLog = [
   {
@@ -30,6 +31,11 @@ let arrayLog = [
 const Approved = () => {
   const navigate = useHistory();
   const appSlice = useSelector((state) => state.appSlice);
+
+  const handlerEditorChange = (value) => {
+    console.log(value)
+  }
+
   useEffect(() => {
     if (!appSlice.isLogin) {
       navigate.push("/login");
@@ -41,6 +47,9 @@ const Approved = () => {
       {arrayLog.map((item, index) => (
         <ApprovedItem item={item} key={item.id} index={index}></ApprovedItem>
       ))}
+
+      <h4>CKEditor Hello</h4>
+      <MyCKEditor onValueChange={handlerEditorChange} />
     </div>
   );
 };
