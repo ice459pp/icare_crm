627f97f58dd5dd39098026e7a1c0d6623c1aa1c9
hsu-feng <feng@iheal.tw>
2024-06-20 09:47:06 +0800

    add qaedit  path

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import { useViewport } from "./useViewport";
 import Counts from "./pages/counts";
 import Service from "./pages/service";
 import Qanda from "./pages/qanda";
+import QandaEdite from "./component/home/qanda-edit"
 import { scrollTopAction } from "./store/scrollTop-slice";
 import { useLayoutEffect } from "react";
 const NavbarWidth = {
@@ -115,6 +116,7 @@ function App() {
                 <Home></Home>{" "}
               </Route>
               <Route path="/clinic/:id" component={ClinicDetail} />
+              <Route path="/qaedit" component={QandaEdite} />
               <Route path="/counts" component={Counts} />
               <Route path="/service" component={Service} />
               <Route path="/approved" component={Approved} />
