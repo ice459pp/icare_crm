c0988eeed6d705e4cb5c56aac22c2b8d3068fd02
ice459pp <ice459pp@gmail.com>
2024-06-25 14:02:33 +0800

    fix CKEditor

--- src/component/qanda/CKEditor.js
+++ src/component/qanda/CKEditor.js
@@ -1,23 +1,22 @@
-import React, {  useEffect, useRef } from 'react';
+import React, { useEffect, useRef } from 'react';
 
 const CKEditor = (props) => {
     const editorRef = useRef(null);
     const { content } = props;
-    console.log('ckEditor popout', content)
- 
+
     useEffect(() => {
-        // 在componentDidMount时加载CKEditor
         const loadCKEditor = () => {
             if (window.CKEDITOR) {
                 const editor = window.CKEDITOR.replace(editorRef.current);
                 editor.on('blur', () => {
                     const data = editor.getData();
-                    props.onValueChange(data)
-                })
+                    props.onValueChange(data);
+                });
+                // 设置初始内容
+                editor.setData(content);
             }
         };
 
-        // 检查CKEditor是否已经加载，如果没有则设置一个监听器
         if (window.CKEDITOR) {
             loadCKEditor();
         } else {
@@ -28,17 +27,27 @@ const CKEditor = (props) => {
         }
 
         // 清理函数，以防止内存泄漏
-        // return () => {
-        //     if (window.CKEDITOR) {
-        //         window.CKEDITOR.instances[editorRef.current.id] && window.CKEDITOR.instances[editorRef.current.id].destroy();
-        //     }
-        // };
+        return () => {
+            if (window.CKEDITOR && editorRef.current) {
+                const instance = window.CKEDITOR.instances[editorRef.current.id];
+                if (instance) {
+                    instance.destroy();
+                }
+            }
+        };
     }, []);
 
-    return (
+    useEffect(() => {
+        if (window.CKEDITOR && editorRef.current) {
+            const instance = window.CKEDITOR.instances[editorRef.current.id];
+            if (instance) {
+                instance.setData(content);
+            }
+        }
+    }, [content]);
 
-        <textarea ref={editorRef} id="editor1" defaultValue={content}></textarea>
-  
+    return (
+        <textarea ref={editorRef} id="editor1"></textarea>
     );
 };
 
