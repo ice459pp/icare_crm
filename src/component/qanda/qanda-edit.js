85e6fbd1fa2797fd8196a85bc5eb6cd9e76579aa
hsu-feng <feng@iheal.tw>
2024-06-25 12:06:28 +0800

    adjust content

--- src/component/qanda/qanda-edit.js
+++ src/component/qanda/qanda-edit.js
@@ -2,16 +2,17 @@ import React, { Fragment, useEffect, useState } from "react";
 import { Button, Form, InputGroup } from "react-bootstrap";
 import { useHistory, useParams } from "react-router-dom";
 import { useSelector } from "react-redux";
-import { apiQaEdit } from "../../api/api-qaEdit";
+import { apiQaEdit } from "../../api/api-qa-info";
+import { apiQaUpdate } from "../../api/api-qa-edit"
 import CKEditor from "./CKEditor";
 
 let qaData = {
-    id:"",
-    title:"",
-    content:"",
-    click:"",
-    edittime:"",
-    open:""
+    id: "",
+    title: "",
+    content: "",
+    click: "",
+    edittime: "",
+    open: ""
 }
 
 const QandaEdit = () => {
@@ -20,37 +21,74 @@ const QandaEdit = () => {
     //const params = useParams();
     const id = 1;
     const [qaInfo, setQaInfo] = useState(qaData);
+    const [apiUpdate, setApiUpdate] = useState(false);
+    const [title, setTitle] = useState(qaData.title);
+    const [content, setContent] = useState(qaData.content);
+    const [edittime, setEdittime] = useState(qaData.edittime);
+    const [open, setOpen] = useState();
+    
+    console.log('QaEdit', title, content, edittime ) //SUCCESS
 
     const handlerEditorChange = (value) => {
-        console.log(value)
-      };
+        console.log(value);
+        setContent(value);
+    };
 
-    const closeHandler = () =>{
+    const closeHandler = () => {
         navigate.push(`/qanda`);
     };
 
+    // GET API-Info
     useEffect(() => {
         if (appSlice.isLogin) {
-        const token = appSlice.userToken;
-        apiQaEdit(
-            token,
-            id,
-            (err) => {
-                console.log("err: " + err)
-            },
-            (data) => {
-                console.log("response: " + data)
-                setQaInfo(data)
-            }
-        )}
-    },[]);
-    console.log(qaInfo) //陣列含全部物件
-    
+            const token = appSlice.userToken;
+            apiQaEdit(
+                token,
+                id,
+                (err) => {
+                    console.log("err: " + err)
+                },
+                (data) => {
+                    console.log("response: " + data)
+                    setQaInfo(data)
+                }
+            )
+        }
+    }, []);
+
+    // POST API-Info
+    useEffect(() => {
+        if (apiUpdate) {
+            const token = appSlice.userToken;
+            apiQaUpdate(
+                token,
+                id,
+                title,
+                content,
+                edittime,
+                open,
+                (err) =>{
+                    alert(err);
+                },
+                () =>{
+                    setApiUpdate(false);
+                    navigate.push('/qanda')
+                }
+            );
+        }
+    }, [apiUpdate])
+
+    const apiUpdateHandler = () => {
+        const newDate = new Date();
+        setEdittime(newDate);
+        setApiUpdate(true)
+    };
+
     return (
         <Fragment>
             <div className="d-flex justify-content-end border-bottom ">
                 <Button className="btn btn-lg m-3">儲存</Button>
-                <Button className="btn btn-lg btn-success m-3">提交</Button>
+                <Button className="btn btn-lg btn-success m-3" onClick={apiUpdateHandler} >送出</Button>
                 <Button className="btn btn-lg btn-dark m-3" onClick={closeHandler}>取消</Button>
             </div>
             <div className="search w-90 m-3">
@@ -58,15 +96,18 @@ const QandaEdit = () => {
                     <div className="w-50">
                         <InputGroup >
                             <Form.Control
-                                placeholder={qaInfo.title}
+                                defaultValue={qaInfo.title}
                                 aria-label="Title"
                                 aria-describedby="basic-addon1"
+                                onChange={(e) => {
+                                    setTitle(e.target.value);
+                                }}
                             />
                         </InputGroup>
                     </div>
                 </div>
                 <div className="p-3">
-                    <CKEditor onValueChange={handlerEditorChange} content={qaInfo}/>
+                    <CKEditor onValueChange={handlerEditorChange} content={qaInfo.content} />
                 </div>
             </div>
         </Fragment>
