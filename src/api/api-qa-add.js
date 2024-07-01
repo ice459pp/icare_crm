948683abc0ca1f3ab7e439ba5c6ff1198d284265
hsu-feng <feng@iheal.tw>
2024-07-01 17:15:05 +0800

    adjust parameter

--- src/api/api-qa-add.js
+++ src/api/api-qa-add.js
@@ -2,12 +2,9 @@ import appConfig from "../app-config.json"
 
 export const apiQaCreate = async (
     token ="",
-    id,
     title,
     category,
     content,
-    click,
-    edittime,
     open,
     onError = () => {},
     onComplete = () => {}
@@ -16,15 +13,11 @@ export const apiQaCreate = async (
     try {
         const apiUrl = `${appConfig.url}/qa/add`;
         let formData = new FormData();
-        formData.append("id", id)
         formData.append("title", title)
         formData.append("category", category)
         formData.append("content", content)
-        formData.append("click", click)
-        formData.append("edittime", edittime)
         formData.append("open", open)
-
-
+        
         const response = await fetch(
             apiUrl, {
             method: 'POST', 
