d0248ea01b9cb4a1ce7c14e152b304571f8a1241
hsu-feng <feng@iheal.tw>
2024-07-03 17:34:18 +0800

    create  api-qa-category

---
+++ src/api/api-qa-category.js
@@ -0,0 +1,30 @@
+import appConfig from "../app-config.json";
+
+export const apiQaCategory = async (
+    token = "",
+    onError = () => {},
+    onComplete = () => {}
+) => {
+    try {
+        const apiUrl = `${appConfig.url}/qa/category`;
+        const response = await fetch(apiUrl, {
+          method: "GET",
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
+        const json = await response.json();
+        if (!response.ok) {
+          throw new Error("Error Occur");
+        }
+        if (json.status) {
+          const data = json.data;
+          console.log(json);
+          onComplete(data);
+        } else {
+          onError(json.error);
+        }
+      } catch (error) {
+        onError(error);
+      }
+};
