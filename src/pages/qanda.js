9e138b766c937b202fdde4e1ab6bfcdc1ca529cd
hsu-feng <feng@iheal.tw>
2024-07-12 10:41:20 +0800

    add question category and delete modal

--- src/pages/qanda.js
+++ src/pages/qanda.js
@@ -1,12 +1,12 @@
 import React, { Fragment, useEffect, useState } from "react";
 import { useHistory } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
-import { Form, Tab, Tabs, Table, Button } from "react-bootstrap";
+import { Form, Tab, Tabs, Table, Button, Modal } from "react-bootstrap";
 import QaFilter from "../component/qanda/qa-filter";
 import { qaFilterAction } from "../store/qa-filter-slice";
-import { apiQaList } from "../api/api-qa-list";
-import { apiQaUpdate } from "../api/api-qa-edit";
-import { apiQaDelete } from "../api/api-qa-delete";
+import { apiQaList } from "../api/api-qa/api-qa-list";
+import { apiQaUpdate } from "../api/api-qa/api-qa-edit";
+import { apiQaDelete } from "../api/api-qa/api-qa-delete";
 
 const Qanda = () => {
     const dispatch = useDispatch();
@@ -18,13 +18,15 @@ const Qanda = () => {
     const [keyword, setKeyword] = useState(qaFilterSlice.searchText);
     const [category, setCategory] = useState('');
     const [key, setKey] = useState('all');
+    const [deleteId, setDeleteId] = useState(null);
+    const [showDeleteModal, setShowDeleteModal] = useState(false);
 
     const searchTextHandler = (value) => {
         dispatch(qaFilterAction.onSearchText(value));
         setKeyword(value);
     };
 
-    const categoryHandler = (value) =>{
+    const categoryHandler = (value) => {
         dispatch(qaFilterAction.onCategory(value));
         console.log(value) //1
         setCategory(value);
@@ -54,13 +56,13 @@ const Qanda = () => {
             fetchQaList();
         }
     }, [
-        appSlice.isLogin, category, keyword ]);
+        appSlice.isLogin, category, keyword]);
 
     // POST switchStatus
-    useEffect(()=>{
-        if(switchChange){
+    useEffect(() => {
+        if (switchChange) {
             const token = appSlice.userToken;
-            const { id, title, content, category, open } = switchChange; 
+            const { id, title, content, category, open } = switchChange;
             apiQaUpdate(
                 token,
                 id,
@@ -99,11 +101,16 @@ const Qanda = () => {
     };
 
     // Delete Item
-    const qandaDeleteHandler = (id) =>{
+    const qandaDeleteHandler = (id) => {
+        setDeleteId(id);
+        setShowDeleteModal(true);
+    }
+
+    const confirmDeleteHandler = () => {
         const token = appSlice.userToken;
         apiQaDelete(
             token,
-            id,
+            deleteId,
             (err) => {
                 alert(err);
             },
@@ -111,15 +118,17 @@ const Qanda = () => {
                 fetchQaList();
             }
         );
+        setShowDeleteModal(false);
     };
 
     const renderTableRows = (filterItems) => (
         filterItems.map((item) => (
             <tr key={item.id}>
                 <td data-th="標題:" className="data-th">{item.title}</td>
-                <td data-th="分類:">{item.category}</td>
+                <td data-th="系統分類:">{item.category}</td>
+                <td data-th="問題分類:">{item.subcategory}</td>
                 <td data-th="編輯時間:">{item.edittime}</td>
-                <td  data-th="是否啟用:">
+                <td data-th="是否啟用:">
                     <Form>
                         <Form.Check
                             type="switch"
@@ -157,13 +166,12 @@ const Qanda = () => {
 
     return (
         <Fragment>
-                <div className="w-100 mt-3 padding-RWD">
-                    <QaFilter
-                        onCategoryChange={categoryHandler}
-                        onSearchText={searchTextHandler}
-                    />
-                </div>
-
+            <div className="w-100 mt-3 padding-RWD">
+                <QaFilter
+                    onCategoryChange={categoryHandler}
+                    onSearchText={searchTextHandler}
+                />
+            </div>
             <div className="w-100 padding-RWD mt-3">
                 <h4 className="text-center fw-bolder "> Q & A </h4>
                 <div className="d-flex align-items-end tableSort mb-2">
@@ -181,7 +189,8 @@ const Qanda = () => {
                                 <thead>
                                     <tr className="bg-secondary text-white tr-only-hide">
                                         <th>標題</th>
-                                        <th>分類</th>
+                                        <th>系統分類</th>
+                                        <th>問題分類</th>
                                         <th>編輯時間</th>
                                         <th>狀態</th>
                                         <th>編輯</th>
@@ -198,7 +207,8 @@ const Qanda = () => {
                                 <thead>
                                     <tr className="bg-secondary text-white tr-only-hide">
                                         <th>標題</th>
-                                        <th>分類</th>
+                                        <th>系統分類</th>
+                                        <th>問題分類</th>
                                         <th>編輯時間</th>
                                         <th>是否啟用</th>
                                         <th>編輯</th>
@@ -215,7 +225,8 @@ const Qanda = () => {
                                 <thead >
                                     <tr className="bg-secondary text-white tr-only-hide">
                                         <th>標題</th>
-                                        <th>分類</th>
+                                        <th>系統分類</th>
+                                        <th>問題分類</th>
                                         <th>編輯時間</th>
                                         <th>是否啟用</th>
                                         <th>編輯</th>
@@ -230,6 +241,25 @@ const Qanda = () => {
                     </Tabs>
                 </div>
             </div>
+            <Modal
+                className="radio-custom"
+                show={showDeleteModal}
+                centered
+                backdrop="static"
+                size="sm"
+                aria-labelledby="contained-modal-title-vcenter">
+                <Modal.Body>
+                    <div className="d-flex  flex-column align-items-center">
+                        <div className="m-3">
+                            <h4> 確定刪除檔案?</h4>
+                        </div>
+                        <div className="d-flex justify-content-end w-100">
+                            <Button variant="success" className="m-2"onClick={confirmDeleteHandler}>確定</Button>
+                            <Button variant="secondary" className="m-2" onClick={() => setShowDeleteModal(false)}>取消</Button>
+                        </div>
+                    </div>
+                </Modal.Body>
+            </Modal>
         </Fragment>
     );
 };
