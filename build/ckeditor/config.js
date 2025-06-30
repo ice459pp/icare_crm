a7eadc1a1fd680df3a5ef5a34c1abfadb17c32cc
ice459pp <ice459pp@gmail.com>
2025-06-30 15:12:36 +0800

    build the release folder

---
+++ build/ckeditor/config.js
@@ -0,0 +1,32 @@
+/**
+ * @license Copyright (c) 2003-2023, CKSource Holding sp. z o.o. All rights reserved.
+ * For licensing, see https://ckeditor.com/legal/ckeditor-oss-license
+ */
+
+CKEDITOR.editorConfig = function( config ) {
+	// Define changes to default configuration here. For example:
+	// config.language = 'fr';
+	// config.uiColor = '#AADC6E';
+	config.extraPlugins = 'richcombo';
+	config.extraPlugins = 'letterspacing';
+	config.removePlugins = 'exportpdf';
+  // config.line_height="1em;1.1em;1.2em;1.3em;1.4em;1.5em";
+	// config.extraPlugins = "lineheight"
+	// config.toolbarGroups = [
+	// 	{ name: 'clipboard',   groups: [ 'clipboard', 'undo' ] },
+	// 	{ name: 'editing',     groups: [ 'find', 'selection', 'spellchecker' ] },
+	// 	{ name: 'links' },
+	// 	{ name: 'insert' },
+	// 	{ name: 'forms' },
+	// 	{ name: 'tools' },
+	// 	{ name: 'document',	   groups: [ 'mode', 'document', 'doctools' ] },
+	// 	{ name: 'others' },
+	// 	'/',
+	// 	{ name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
+	// 	{ name: 'paragraph',   groups: [ 'list', 'indent', 'blocks', 'align', 'bidi' ] },
+	// 	{ name: 'styles' },
+	// 	{ name: 'colors' },
+	// 	{ name: 'about' }, 
+		//{ name: 'lineheight' }
+	// ];
+};
